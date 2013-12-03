db540b9d70add40547f5e3f6a7354dfcf3b89eb2
Yee Chin <YeeChiam@teamaol.com>
2013-12-03 17:00:43 -0500

    asd

---
+++ src/main/webapp/js/functions.js
@@ -0,0 +1,33 @@
+function spinWheel() {
+	$("#wheel").rotate({
+		duration : 6000,
+		angle : 0,
+		animateTo : 1500 + Math.random() * 1500,
+		easing : $.easing.easeInOutElastic,
+		callback : function() {
+			getParam($(this).getRotateAngle() % 360);
+		}
+	});
+}
+
+function getParam(deg) {
+	if (deg >= 270 || deg < 30) {
+		param = "highTemp";
+		parameter = 'High Temperature';
+	} else if (deg >= 30 && deg < 150) {
+		param = "windWpeed";
+		parameter = 'Wind Speed';
+	} else {
+		param = "lowTemp";
+		parameter = 'Low Temperature';
+	}
+
+	document.getElementById("pp").innerHTML = 'Guess the '
+			+ parameter;
+
+	return (param);
+}
+
+function myFunction() {
+	alert("test");
+}
